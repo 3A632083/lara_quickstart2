92dafa5efa617692cc4648435cc28b06d520dfcd
3A632083 <kiki87119@gmail.com>
2019-12-13 15:57:09 +0800

    建立路由

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,12 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+Route::get('/home', 'HomeController@index')->name('home');
+// 認證路由...
+Route::auth();
+
+Route::get('/tasks', 'TaskController@index');
+Route::post('/task', 'TaskController@store');
+Route::delete('/task/{task}', 'TaskController@destroy');
